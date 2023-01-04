17358ce4c05b57cbf9e57521bbf13e3270fd90e7
jeongyolee <jeongyolee@ebay.com>
2023-01-04 20:00:43 +0900

    fix: add Transactional annotation

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -4,6 +4,8 @@ import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 public class StockService {
 
@@ -13,6 +15,7 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
+    @Transactional
     public void decrease(Long id, Long quantity) {
         //get stock
         Stock stock = stockRepository.findById(id).orElseThrow();
